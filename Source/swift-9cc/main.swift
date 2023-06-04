f8461db1296dc37835b6b376d2d56e4275172c8a
tokizuoh <tokizuoh@gmail.com>
2023-06-04 15:58:47 +0900

    chore

--- Source/swift-9cc/main.swift
+++ Source/swift-9cc/main.swift
@@ -1,32 +1,20 @@
-guard CommandLine.arguments.count == 2 else {
-    error("引数の個数が正しくありません")
-}
+func main() {
+    guard CommandLine.arguments.count == 2 else {
+        error("引数の個数が正しくありません")
+    }
 
-let inputText = CommandLine.arguments[1]
+    let inputText = CommandLine.arguments[1]
 
-let tokens = Tokenizer.tokenize(text: inputText)
+    let tokens = Tokenizer.tokenize(text: inputText)
 
-print(".intel_syntax noprefix")
-print(".globl main")
-print("main:")
+    print(".intel_syntax noprefix")
+    print(".globl main")
+    print("main:")
 
-var isFirst = true
-var tmpOperator: TokenKind.Operator? = nil
-for token in tokens {
-    if isFirst {
-        guard case .number(let value) = token.kind else {
-            errorAt(
-                "数ではありません",
-                inputText: inputText,
-                offset: token.position
-            )
-        }
-
-        print("  mov rax, \(value)")
-
-        isFirst = false
-    } else {
-        if let _tmpOperator = tmpOperator {
+    var isFirst = true
+    var tmpOperator: TokenKind.Operator? = nil
+    for token in tokens {
+        if isFirst {
             guard case .number(let value) = token.kind else {
                 errorAt(
                     "数ではありません",
@@ -35,22 +23,39 @@ for token in tokens {
                 )
             }
 
-            switch _tmpOperator {
-                case .plus:
-                    print("  add rax, \(value)")
-                case .minus:
-                    print("  sub rax, \(value)")
-            }
-            tmpOperator = nil
-        } else {
-            guard case .reserved(let ope) = token.kind else {
-                error("+か-の符号ではありません")
-            }
+            print("  mov rax, \(value)")
 
-            tmpOperator = ope
+            isFirst = false
+        } else {
+            if let _tmpOperator = tmpOperator {
+                guard case .number(let value) = token.kind else {
+                    errorAt(
+                        "数ではありません",
+                        inputText: inputText,
+                        offset: token.position
+                    )
+                }
+
+                switch _tmpOperator {
+                    case .plus:
+                        print("  add rax, \(value)")
+                    case .minus:
+                        print("  sub rax, \(value)")
+                }
+                tmpOperator = nil
+            } else {
+                guard case .reserved(let ope) = token.kind else {
+                    error("+か-の符号ではありません")
+                }
+
+                tmpOperator = ope
+            }
         }
+
     }
 
+    print("  ret")
 }
 
-print("  ret")
+
+main()
